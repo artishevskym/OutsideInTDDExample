1eb1b7d0279acf77835b3528d766eca2cc515d45
Martin Artishevsky <artishevsky.m@gmail.com>
2022-01-03 14:11:42 +0000

    Red unit test

--- app/src/test/java/com/example/outsideintddexample/unittest/EngineShould.kt
+++ app/src/test/java/com/example/outsideintddexample/unittest/EngineShould.kt
@@ -1,6 +1,7 @@
 package com.example.outsideintddexample.unittest
 
 import com.example.outsideintddexample.Engine
+import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
@@ -13,5 +14,12 @@ class EngineShould {
         engine.turnOn()
 
         assertTrue(engine.isTurnedOn)
+    }
+
+    @Test
+    fun riseTheTemperatureWhenItTurnsOn() {
+        engine.turnOn()
+
+        assertEquals(95, engine.temperature)
     }
 }
