0101bf85442c9f76fe3885ad3273c337fb8a9a70
Martin Artishevsky <artishevsky.m@gmail.com>
2022-01-03 13:12:59 +0000

    Second failing acceptance test

--- app/src/test/java/com/example/outsideintddexample/acceptancetest/CarFeature.kt
+++ app/src/test/java/com/example/outsideintddexample/acceptancetest/CarFeature.kt
@@ -2,6 +2,7 @@ package com.example.outsideintddexample.acceptancetest
 
 import com.example.outsideintddexample.Car
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
 
 import org.junit.Test
 
@@ -14,5 +15,13 @@ class CarFeature {
         car.turnOn()
 
         assertEquals(5.5, car.fuel)
+    }
+
+    @Test
+    fun carIsTurningOnItsEngineAndIncreasesTheTemperature() {
+        car.turnOn()
+
+        assertTrue(car.engine.isTurnedOn)
+        assertEquals(95, car.engine.temperature)
     }
 }
